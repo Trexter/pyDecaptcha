8a6854475f0614ed05b33c4b9eb72d12b2c96db0
Kevin Sheridan <sheridak@purdue.edu>
2016-06-15 14:08:22 -0400

    using tesseract

--- appleIDMaker.py
+++ appleIDMaker.py
@@ -1,18 +1,23 @@
 import numpy
 import cv2
 import time
+import pytesser
 
-print("Automatic Apple ID Creator")
+print "Automatic Apple ID Creator\n"
 
-index = 1
+index = 24
 
-while index < 23:
+while index < 26:
 
 	path = "captcha/captcha" + str(index) + ".jpg"
 
 	img = cv2.imread(path)
 	gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	
+
+	#detect text in the image
+	text = pytesser.mat_to_string(gray_img)
+
+	print(text)
 
 	cv2.imshow('Image', gray_img)
 	key = cv2.waitKey(113)
